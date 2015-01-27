634cf12acfa95b764a2b19fc804fe78874934da3
Licud <leinmik.ML@gmail.com>
2015-01-27 07:08:46 +0000

    Initial Commit; Default MVC 4.5 Application with Individual User Accounts

---
+++ TimesheetSystem/TimesheetSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimesheetSystem.Startup))]
+namespace TimesheetSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
